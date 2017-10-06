58b70b7c897d4ad3ed8a8fdca53a21dd5596b84a
Thomas Perelle <t.perelle@gmail.com>
2017-10-06 21:40:01 +0200

    Add comment

--- student_provisionning/Azure/summary.sh
+++ student_provisionning/Azure/summary.sh
@@ -6,13 +6,24 @@ source ./env.sh
 # rm old summary file
 SUMMARY_FILE="summary.txt"
 rm -f $SUMMARY_FILE
+echo " " >> $SUMMARY_FILE
+echo "#######################################################" >> $SUMMARY_FILE
+echo "# Formation : $FORM_NAME" >> $SUMMARY_FILE
+echo "# Formateur : $FORM_TEACHER_NAME ($FORM_TEACHER_MAIL)" >> $SUMMARY_FILE
+echo "# Cloud : Azure" >> $SUMMARY_FILE
+echo "# Groupe de ressources : $AZURE_RESSOURCES_GROUP" >> $SUMMARY_FILE
+echo "#######################################################" >> $SUMMARY_FILE
+echo " " >> $SUMMARY_FILE
 
 # Environment summary
+echo "Machines virtuelles :" >> $SUMMARY_FILE
 vm=$(az vm list --resource-group $AZURE_RESSOURCES_GROUP --query "[*].[name]" --out tsv)
 for i in $vm
 do
   ip=$(az network public-ip show -g $AZURE_RESSOURCES_GROUP -n ${i}PublicIP --query "[ipAddress]" --out tsv)
   echo "$i $ip" >> $SUMMARY_FILE
 done
+
+echo " " >> $SUMMARY_FILE
 
 cat $SUMMARY_FILE
