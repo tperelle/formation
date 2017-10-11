c7d8fa08709506ba35f84e4a1d4f429ae3af3bf1
tperelle <t.perelle@gmail.com>
2017-10-11 14:42:07 +0200

    Update for ansible

--- student_provisioning/DigitalOcean/provisioning.sh
+++ student_provisioning/DigitalOcean/provisioning.sh
@@ -45,3 +45,8 @@ do
 
   let "itStudent+=1"
 done
+
+echo "[student:vars]" >> $ANSIBLE_HOST_FILE
+echo "ansible_ssh_private_key_file=$ANSIBLE_SSH_KEY" >> $ANSIBLE_HOST_FILE
+echo "ansible_become_user=$ANSIBLE_REMOTE_USER" >> $ANSIBLE_HOST_FILE
+echo "ansible_become_pass=$ANSIBLE_REMOTE_PASSWD" >> $ANSIBLE_HOST_FILE
